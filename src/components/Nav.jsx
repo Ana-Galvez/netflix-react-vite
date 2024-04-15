c38c430c0d3196b4c799d1d070b4030bbfeac2ad
Ana-Galvez <anamariagalvez01@gmail.com>
2024-04-15 17:08:31 -0300

    comienzo de nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,8 +4,15 @@ import '../styled/Nav.css';
 const Nav = () => {
   return (
     <div className='nav'>
-      <img className='nav__logo' src="../../public/image/logo-netflix.png" alt="Logo de Netflix" />
-      <img className='nav__avatar' src="../../public/image/Netflix-avatar.png" alt="Avatar de usuario" />
+      <img className='nav__logo' src="/image/logo-netflix.png" alt="Logo de Netflix" />
+      {/* <div className='nav__menu'>
+        <ul>
+          <a href="/">Home</a>
+          <a href="/peliculas">Películas</a>
+          <a href="/series">Series</a>
+        </ul>
+      </div> */}
+      <img className='nav__avatar' src="/image/Netflix-avatar.png" alt="Avatar de usuario" />
     </div>
   )
 }
